7f375b7e9c0f9774abcfd2a984cd6e33c6e51bf4
ELMbadre <badreddineelmoutarif@gmail.com>
2024-02-02 01:26:28 +0100

    All tasks finished! 🏁

---
+++ char.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * f_pchar - print the char at the top of stack
+ * @head: stack head
+ * @counter: line counter
+ *
+ * Return: none
+ */
+
+void f_pchar(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	h = *head;
+	if (!h)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	if (h->n > 127 || h->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", h->n);
+}
